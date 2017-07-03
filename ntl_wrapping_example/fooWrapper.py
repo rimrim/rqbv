e658d770a71bbc6050ecb90ba986990b5c386657
Trung Dinh <trung@infosec.org.au>
2017-07-03 05:05:16 +0000

    add instruction

--- ntl_wrapping_example/fooWrapper.py
+++ ntl_wrapping_example/fooWrapper.py
@@ -1,3 +1,9 @@
+# compiling instruction:
+# g++ -c -fPIC foo.cpp -o foo.o -lntl
+# g++ -shared -Wl,-soname,libfoo.so -o libfoo.so foo.o -lntl
+# https://stackoverflow.com/questions/145270/calling-c-c-from-python
+
+
 from ctypes import cdll
 lib = cdll.LoadLibrary('./libfoo.so')
 
@@ -11,7 +17,10 @@ class Foo(object):
     def test_ntt(self):
         lib.test_ntt(self.obj)
 
+    def test_speed(self):
+        lib.test_speed(self.obj)
+
 f = Foo()
 f.bar()
-f.test_ntt()
-
+# f.test_ntt()
+f.test_speed()
