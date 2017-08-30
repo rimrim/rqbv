b99c0796233b6d52937086c769d5b1063e75ee63
Trung Dinh <trung@infosec.org.au>
2017-08-31 02:38:18 +1000

    start utility class Auth_protocol to measure time and communication size

--- test_variant_1.py
+++ test_variant_1.py
@@ -2,13 +2,39 @@ import unittest
 from bv import poly_multiply, BV, Rq, modmath, \
     small_samples, large_samples, rot
 from timer import Timer
+import time
 from bitarray import bitarray
 from math import log,ceil,floor
+
+class AuthProtocol(object):
+    def __init__(self):
+        self.timer = Timer()
+        self.steps = []
+        self.start = None
+        self.end = None
+
+    def start_time_for(self, step):
+        if step not in self.steps:
+            temp_dict = {'key':step,'comp_time':None,'comm_size':None}
+            self.steps.append(temp_dict)
+        self.start = time.time()
+
+
 
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)
+
+    def test_record_time(self):
+        auth = AuthProtocol()
+        auth.start_time_for("sample")
+        a = 2
+        b = 200
+        c = a**b
+        auth.stop_time_for("sample")
+        t = auth.time_for("sample")
+        print(t)
 
     def test_whole_protocol(self):
         # init parameter for the authentication system
@@ -26,9 +52,12 @@ class MyTestCase(unittest.TestCase):
         # alice registers her fingerprint
 
         # alice extracts her iris code
-        registered_template = [1 for _ in range(n)]
+        T = [1 for _ in range(n)]
+        T = Rq(n = n, q = t, coeffs = T)
 
         # alice encrypts the registered template
+        pack1 = bv.pack1(T)
+        enc_T = bv.enc(pack1, pk_a)
 
         # alice sends the ciphertext to bob
 
